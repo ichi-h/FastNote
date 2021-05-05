37ad9f4e29bf9112da608b99f96b3d87dcad7734
ippee <ippee.music@gmail.com>
2021-05-05 23:26:05 +0900

    jestの追加

--- jest.config.js
+++ jest.config.js
@@ -6,16 +6,9 @@ module.exports = {
   // Jest transformations -- this adds support for TypeScript
   // using ts-jest
   transform: {
-    "^.+\\.tsx?$": "ts-jest",
+    "^.+\\.tsx$": "babel-jest",
+    "^.+\\.tsx$": "ts-jest",
   },
-
-  // Runs special logic, such as cleaning up components
-  // when using React Testing Library and adds special
-  // extended assertions to Jest
-  setupFilesAfterEnv: [
-    "@testing-library/react/cleanup-after-each",
-    "@testing-library/jest-dom/extend-expect",
-  ],
 
   // Test spec file resolution pattern
   // Matches parent folder `__tests__` and filename
