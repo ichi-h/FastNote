778856728718204970f7cc7ae6b1bd4756559b13
ippee <ippee.music@gmail.com>
2021-05-08 14:28:23 +0900

    コンポーネント追加

---
+++ src/components/memo/memoList.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+import css from "styled-jsx/css";
+
+export default function MemoList() {
+  return (
+    <>
+      <Head>
+        <title>Fast Note</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      MemoList
+    </>
+  );
+}
