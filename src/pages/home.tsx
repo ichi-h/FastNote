f8092c78ed81a24da67f290b44661659273559a9
ippee <ippee.music@gmail.com>
2021-05-08 14:28:34 +0900

    コンポーネント名変更

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Head from "next/head";
 import css from "styled-jsx/css";
 
-export default function Home() {
+export default function HomeCampus() {
   return (
     <>
       <Head>
