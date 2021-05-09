866c1341c5073574dfd0867f17bf24bba3ef02ef
ippee <ippee.music@gmail.com>
2021-05-09 14:00:50 +0900

    checked===true時に横からnavbarが出現

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,15 +1,40 @@
 import React from "react";
-import Head from "next/head";
 import css from "styled-jsx/css";
+import { useRecoilValue } from "recoil";
+
+import { openNavbarState } from "../pages/home";
 
 export default function Navbar() {
+  const checked = useRecoilValue(openNavbarState);
+
   return (
     <>
-      <Head>
-        <title>Fast Note</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      Navbar
+      <div className="navbar">aaa</div>
+
+      {navbarStyle(checked)}
     </>
   );
 }
+
+const navbarStyle = (checked: boolean) => {
+  const leftPos = (bool: boolean) => {
+    if (bool) return "0";
+    else return "-20vw";
+  };
+
+  return (
+    <style jsx>{`
+      .navbar {
+        position: absolute;
+        top: 0;
+        left: ${leftPos(checked)};
+        width: 20vw;
+        height: 100%;
+        border: 1px solid black;
+        background-color: white;
+        transition: 0.3s;
+        z-index: 1000;
+      }
+    `}</style>
+  );
+};
