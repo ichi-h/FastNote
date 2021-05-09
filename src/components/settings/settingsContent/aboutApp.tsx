2f1d3d3cf2709b63e69b5c5f9ef78717c45b1b96
ippee <ippee.music@gmail.com>
2021-05-09 21:38:57 +0900

    SettingsContentの追加

---
+++ src/components/settings/settingsContent/aboutApp.tsx
@@ -0,0 +1,6 @@
+import Route from "react-router-dom";
+import css from "styled-jsx/css";
+
+export default function AboutApp() {
+  return <>AboutApp</>;
+}
---
+++ src/components/settings/settingsContent/userSettings.tsx
@@ -0,0 +1,6 @@
+import Route from "react-router-dom";
+import css from "styled-jsx/css";
+
+export default function UserSettings() {
+  return <>UserSettings</>;
+}
