66e74893718e75e6d16ef71fe16316d5a75ef01c
ippee <ippee.music@gmail.com>
2021-05-09 11:44:51 +0900

    メモ一覧取得機能の仮実装

---
+++ src/lib/getMemo.ts
@@ -0,0 +1,42 @@
+export default function getMemo() {
+  const memo = [
+    {
+      title: "sample1",
+      category: "sample1",
+      tag: ["a", "b", "c"],
+      favorite: false,
+      creationDate: 20210501,
+      updateDate: 20210504,
+      content: "",
+    },
+    {
+      title: "sample1",
+      category: "sample1",
+      tag: ["a", "b", "c"],
+      favorite: false,
+      creationDate: 20210501,
+      updateDate: 20210504,
+      content: "",
+    },
+    {
+      title: "sample2",
+      category: "sample1",
+      tag: ["d", "e", "f"],
+      favorite: false,
+      creationDate: 20210502,
+      updateDate: 20210505,
+      content: "",
+    },
+    {
+      title: "sample3",
+      category: "sample1",
+      tag: ["a", "b", "c"],
+      favorite: false,
+      creationDate: 2021053,
+      updateDate: 20210506,
+      content: "",
+    },
+  ];
+
+  return memo;
+}
