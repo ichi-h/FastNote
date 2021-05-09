d0a41c38ed45a7f472c2e0ef164314a0d9994a4a
ippee <ippee.music@gmail.com>
2021-05-09 11:17:31 +0900

    SettingsItemコンポーネントの追加

---
+++ src/components/settings/settingsItem.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Head from "next/head";
+import css from "styled-jsx/css";
+
+export default function SettingsItem() {
+  return (
+    <>
+      <Head>
+        <title>Fast Note</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      SettingsItem
+    </>
+  );
+}
