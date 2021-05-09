1a1d0daddf92d39b46610bf40158a935cb831ad7
ippee <ippee.music@gmail.com>
2021-05-09 16:42:13 +0900

    コンポーネント分割

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,9 +5,53 @@ import { useRecoilValue } from "recoil";
 import { getCategories } from "../lib/getMemo";
 import { openNavbarState } from "../pages/home";
 
+function CategoriesCheckbox(props: {
+  categoriesChecked: boolean;
+  clickHandle: () => void;
+}) {
+  return (
+    <>
+      <label htmlFor="categories-checkbox" className="checkbox-label">
+        <input
+          type="checkbox"
+          className="categories-checkbox"
+          name="categories-checkbox"
+          id="categories-checkbox"
+          defaultChecked={props.categoriesChecked}
+          onClick={props.clickHandle}
+        />
+        カテゴリー
+      </label>
+
+      <style jsx>{categoriesCheckboxStyle}</style>
+    </>
+  );
+}
+
+function CategoriesList(props: { categoriesChecked: boolean }) {
+  const [categories, count] = getCategories();
+
+  return (
+    <>
+      <div className="categories-list">
+        <ul>
+          {categories.map((category, i) => {
+            return (
+              <li className="category">
+                {category} ({count[i]})
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+
+      {categoriesListStyle(props.categoriesChecked)}
+    </>
+  );
+}
+
 export default function Navbar() {
   const checked = useRecoilValue(openNavbarState);
-  const [categories, count] = getCategories();
 
   const [categoriesChecked, toggle] = useState(true);
 
@@ -19,46 +63,24 @@ export default function Navbar() {
     <>
       <div className="navbar">
         <div className="categories">
-          <label htmlFor="categories-checkbox" className="checkbox-label">
-            <input
-              type="checkbox"
-              className="categories-checkbox"
-              name="categories-checkbox"
-              id="categories-checkbox"
-              defaultChecked={categoriesChecked}
-              onClick={clickHandle}
-            />
-            カテゴリー
-          </label>
+          <CategoriesCheckbox
+            categoriesChecked={categoriesChecked}
+            clickHandle={clickHandle}
+          />
 
-          <div className="categories-list">
-            <ul>
-              {categories.map((category, i) => {
-                return (
-                  <li className="category">
-                    {category} ({count[i]})
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+          <CategoriesList categoriesChecked={categoriesChecked} />
         </div>
       </div>
 
-      {navbarStyle(checked, categoriesChecked)}
+      {navbarStyle(checked)}
     </>
   );
 }
 
-const navbarStyle = (checked: boolean, categoriesChecked: boolean) => {
+const navbarStyle = (checked: boolean) => {
   const leftPos = (bool: boolean) => {
     if (bool) return "0";
     else return "-20vw";
-  };
-
-  const showCategories = (bool: boolean) => {
-    if (bool) return "initial";
-    else return "none";
   };
 
   return (
@@ -78,26 +100,41 @@ const navbarStyle = (checked: boolean, categoriesChecked: boolean) => {
       .categories {
         margin-top: 2rem;
       }
-
-      .checkbox-label {
-        font-size: 2rem;
-        margin-left: 2rem;
-      }
-
-      .categories-checkbox {
-        display: none;
-      }
-
-      .categories-list {
-        display: ${showCategories(categoriesChecked)};
-        transition: 0.3s;
-      }
-
-      .category {
-        font-size: 2rem;
-        margin-left: 4rem;
-        list-style: none;
-      }
     `}</style>
+  );
+};
+
+const categoriesCheckboxStyle = css`
+  .checkbox-label {
+    font-size: 2rem;
+    margin-left: 2rem;
+  }
+
+  .categories-checkbox {
+    display: none;
+  }
+`;
+
+const categoriesListStyle = (categoriesChecked: boolean) => {
+  const showCategories = (bool: boolean) => {
+    if (bool) return "initial";
+    else return "none";
+  };
+
+  return (
+    <style jsx>
+      {`
+        .categories-list {
+          display: ${showCategories(categoriesChecked)};
+          transition: 0.3s;
+        }
+
+        .category {
+          font-size: 2rem;
+          margin-left: 4rem;
+          list-style: none;
+        }
+      `}
+    </style>
   );
 };
