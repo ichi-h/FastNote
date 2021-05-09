d9139add6e726c1c5f1ab0c349e30b2bd9d72d52
ippee <ippee.music@gmail.com>
2021-05-09 13:44:54 +0900

    RecoilRoot導入

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import type { AppProps } from "next/app";
+import { RecoilRoot } from "recoil"
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+  return (
+  <RecoilRoot>
+    <Component {...pageProps} />
+  </RecoilRoot>
+);
 }
 
 export default MyApp;
