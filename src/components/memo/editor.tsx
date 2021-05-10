9bc365fbddbdd832afcb340e19f1e43f68074d83
ippee <ippee.music@gmail.com>
2021-05-10 20:58:30 +0900

    CodeMirror導入, #4

--- src/components/memo/editor.tsx
+++ src/components/memo/editor.tsx
@@ -1,9 +1,22 @@
+import { UnControlled as CodeMirror } from "react-codemirror2";
 import css from "styled-jsx/css";
+
+import "codemirror/lib/codemirror.css";
+import "codemirror/theme/base16-light.css";
 
 export default function Home() {
   return (
     <>
-      <div className="editor">Editor</div>
+      <div className="editor">
+        <CodeMirror
+          value={""}
+          options={{
+            mode: "gfm",
+            theme: "base16-light",
+            lineNumbers: false,
+          }}
+        />
+      </div>
 
       <style jsx>{editorStyle}</style>
     </>
