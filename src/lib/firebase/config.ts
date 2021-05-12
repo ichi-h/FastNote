0e9dc566d6b9bfcfb5c07897c17e0e8d55cdec99
ippee <ippee.music@gmail.com>
2021-05-12 19:04:27 +0900

    firebaseConfigの更新

--- src/lib/firebase/config.ts
+++ src/lib/firebase/config.ts
@@ -1,12 +1,12 @@
 import firebase from "firebase/app";
 
-const firebaseConfig = {
+var firebaseConfig = {
   apiKey: "AIzaSyB1nM54e24m7CYQPfWg2t8yBd3mW_VCugs",
   authDomain: "fast-note-3939b.firebaseapp.com",
+  databaseURL: "https://fast-note-3939b-default-rtdb.firebaseio.com",
   projectId: "fast-note-3939b",
-  storageBucket: "fast-note-3939b.appspot.com",
   messagingSenderId: "641235894549",
-  appId: "1:641235894549:web:786ddc79b74f4b7a5be91c",
+  appId: "1:641235894549:web:786ddc79b74f4b7a5be91c"
 };
 
 if (!firebase.apps.length) {
