ae842f1f4f13ec871e59869aa3d6f83a7be0c67a
ippee <ippee.music@gmail.com>
2021-05-13 13:09:23 +0900

    memoStateの追加, #12

--- src/lib/atoms/editorAtoms.ts
+++ src/lib/atoms/editorAtoms.ts
@@ -1,1 +1,17 @@
 import { atom } from "recoil";
+
+import { Memo } from "../databaseInfo";
+
+// 選択中のメモの情報
+export const memoState = atom({
+  key: "memoState",
+  default: {
+    title: "",
+    category: "",
+    tags: [""],
+    star: false,
+    created: "",
+    updated: "",
+    content: "",
+  } as Memo,
+});
