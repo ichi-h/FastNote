f298e1d184548ca1e81601088da37eb3b174dc8c
ippee <ippee.music@gmail.com>
2021-05-13 19:46:38 +0900

    localDBとカテゴリーを連動, #12

--- src/components/memo/editor/memoCategory.tsx
+++ src/components/memo/editor/memoCategory.tsx
@@ -1,6 +1,29 @@
+import { useRef, useEffect } from "react";
 import { css } from "styled-jsx/css";
+import { useRecoilValue } from "recoil";
+
+import { memoIndexState } from "../../../lib/atoms/editorAtoms";
+
 
 export default function MemoCategory() {
+  const memoIndex = useRecoilValue(memoIndexState);
+  const localDB = JSON.parse(localStorage.getItem("database"));
+  const selectRef: React.LegacyRef<HTMLSelectElement> = useRef();
+
+  useEffect(() => {
+    selectRef.current.value = localDB.memos[memoIndex].category;
+  });
+
+  let categories: string[] = [];
+  for (let i = 0; i < Object.keys(localDB.categories).length; i++) {
+    categories.push(localDB.categories[i]);
+  }
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    localDB.memos[memoIndex].category = e.currentTarget.value;
+    localStorage.setItem("database", JSON.stringify(localDB));
+  }
+
   return (
     <>
       <div className="memo-category">
@@ -8,10 +31,15 @@ export default function MemoCategory() {
           className="category-area"
           name="category-area"
           id="category-area"
+          ref={selectRef}
+          defaultValue={localDB.memos[memoIndex].category}
+          onChange={handleChange}
         >
-          <option value="sample1">sample1</option>
-          <option value="sample2">sample2</option>
-          <option value="sample3">sample3</option>
+          {
+            categories.map((category, i) => {
+              return <option value={category} key={i}>{category}</option>;
+            })
+          }
         </select>
       </div>
 
