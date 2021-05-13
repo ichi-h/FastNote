6baf9504b3e7b12b27b9b41be545df7e7b4fb190
ippee <ippee.music@gmail.com>
2021-05-13 21:10:32 +0900

    localDBとcontentを連動, #12

--- src/components/memo/editor/codeMirrorWrap.tsx
+++ src/components/memo/editor/codeMirrorWrap.tsx
@@ -1,22 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { css } from "styled-jsx/css";
 import { UnControlled as CodeMirror } from "react-codemirror2";
+import { useRecoilValue } from "recoil";
+
+import { memoIndexState } from "../../../lib/atoms/editorAtoms";
 
 import "codemirror/lib/codemirror.css";
 import "codemirror/theme/base16-light.css";
 import "codemirror/mode/markdown/markdown";
 
 const CodeMirrorWrap = React.memo(() => {
+  const memoIndex = useRecoilValue(memoIndexState);
+  const localDB = JSON.parse(localStorage.getItem("database"));
+  let content = localDB.memos[memoIndex].content;
+
+  const handleChangeContent = (_editor: CodeMirror.Editor, _editorChange: CodeMirror.EditorChange, newContent: string) => {
+    localDB.memos[memoIndex].content = newContent;
+    localStorage.setItem("database", JSON.stringify(localDB));
+  };
+
   return (
     <>
       <div className="codemirror-wrap">
         <CodeMirror
-          value={""}
+          value={content}
           options={{
             mode: "markdown",
             theme: "base16-light",
             lineNumbers: false,
           }}
+          onChange={handleChangeContent}
         />
       </div>
 
