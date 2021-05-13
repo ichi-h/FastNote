3c07dfde4914998c166caf536292283228bb5658
ippee <ippee.music@gmail.com>
2021-05-13 20:35:36 +0900

    localDBとtagsを連動, #12

--- src/components/memo/editor/memoTags.tsx
+++ src/components/memo/editor/memoTags.tsx
@@ -1,6 +1,34 @@
+import { useRef, useEffect } from "react";
 import { css } from "styled-jsx/css";
+import { useRecoilValue } from "recoil";
+
+import { memoIndexState } from "../../../lib/atoms/editorAtoms";
+import React from "react";
 
 export default function MemoTags() {
+  const memoIndex = useRecoilValue(memoIndexState);
+  const localDB = JSON.parse(localStorage.getItem("database"));
+  const tagsRef: React.RefObject<HTMLInputElement> = useRef();
+  let tags: string[] = [];
+
+  const updateTags = () => {
+    let newTags : string[] = [];
+    for (let i = 0; i < Object.keys(localDB.memos[memoIndex].tags).length; i++) {
+      newTags.push(localDB.memos[memoIndex].tags[i]);
+    }
+    tags = newTags;
+  };
+
+  useEffect(() => {
+    updateTags();
+    tagsRef.current.value = tags.join(", ");
+  });
+
+  const handleChangeTags = (e: React.ChangeEvent<HTMLInputElement>) => {
+    localDB.memos[memoIndex].tags = e.currentTarget.value.split(", ");
+    localStorage.setItem("database", JSON.stringify(localDB));
+  };
+
   return (
     <>
       <div className="memo-tags">
@@ -9,6 +37,9 @@ export default function MemoTags() {
           className="tags-area"
           name="tags-area"
           id="tags-area"
+          ref={tagsRef}
+          defaultValue={tags.join(", ")}
+          onChange={handleChangeTags}
         />
       </div>
 
