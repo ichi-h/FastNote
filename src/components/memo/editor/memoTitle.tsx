138ffe297df52f3eeec5795129a56c03f61d2453
ippee <ippee.music@gmail.com>
2021-05-13 20:10:13 +0900

    localDBとtitleを連動, #12

--- src/components/memo/editor/memoTitle.tsx
+++ src/components/memo/editor/memoTitle.tsx
@@ -1,6 +1,24 @@
+import { useRef, useEffect } from "react";
 import { css } from "styled-jsx/css";
+import { useRecoilValue } from "recoil";
+
+import { memoIndexState } from "../../../lib/atoms/editorAtoms";
+import React from "react";
 
 export default function MemoTitle() {
+  const memoIndex = useRecoilValue(memoIndexState);
+  const localDB = JSON.parse(localStorage.getItem("database"));
+  const titleRef: React.RefObject<HTMLInputElement> = useRef();
+
+  useEffect(() => {
+    titleRef.current.value = localDB.memos[memoIndex].title;
+  });
+
+  const handleChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    localDB.memos[memoIndex].title = e.currentTarget.value;
+    localStorage.setItem("database", JSON.stringify(localDB));
+  };
+
   return (
     <>
       <div className="memo-title">
@@ -9,6 +27,9 @@ export default function MemoTitle() {
           className="title-area"
           name="title-area"
           id="title-area"
+          ref={titleRef}
+          defaultValue={localDB.memos[memoIndex].title}
+          onChange={handleChangeTitle}
         />
       </div>
 
