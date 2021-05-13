a1af9041fe648fc018fcebd7c2f4770bccdd236c
ippee <ippee.music@gmail.com>
2021-05-14 00:15:03 +0900

    アプリ情報の追加, close #6

--- src/components/settings/settingsContent/aboutApp.tsx
+++ src/components/settings/settingsContent/aboutApp.tsx
@@ -1,6 +1,37 @@
-import Route from "react-router-dom";
 import { css } from "styled-jsx/css";
 
+import theme from "../../../lib/theme";
+
 export default function AboutApp() {
-  return <>AboutApp</>;
+  return (
+    <>
+      <div className="about-app">
+        <h2>Fast Note</h2>
+        <p>Copyright &copy; 2021 Ippee</p>
+        <p>このアプリケーションは<a href="https://www.mozilla.org/en-US/MPL/2.0/" target="_blank" rel="noopener noreferrer">Mozilla Public License 2.0</a>の下で公開されています。</p>
+        <p>GitHub: <a href="https://github.com/ippee/FastNote" target="_blank" rel="noopener noreferrer">ippee / FastNote</a></p>
+      </div>
+
+      <style jsx>{aboutAppStyle}</style>
+    </>
+  );
 }
+
+const aboutAppStyle = css`
+  .about-app {
+    padding: 3rem;
+  }
+
+  h2 {
+    font-size: 1.5rem;
+  }
+
+  p {
+    margin: 0.5rem 0;
+  }
+
+  a {
+    text-decoration: dotted;
+    color: ${theme.subColor};
+  }
+`;
