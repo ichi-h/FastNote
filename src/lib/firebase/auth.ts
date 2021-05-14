985a100683b2347c3e959f14f44483250e3be6cf
ippee <ippee.music@gmail.com>
2021-05-14 19:56:24 +0900

    startUiAuthを初期化

--- src/lib/firebase/auth.ts
+++ src/lib/firebase/auth.ts
@@ -1,10 +1,6 @@
-import router from "next/router";
-import { SetterOrUpdater } from "recoil";
 import firebase from "firebase/app";
 
 import "firebase/auth";
-
-import { FastNoteDatabase, createNewLocalDB } from "../firebase/database";
 
 export async function startUiAuth() {
   if (typeof window !== "undefined") {
@@ -24,7 +20,7 @@ export async function startUiAuth() {
       signInFlow: "popup",
       // tosUrl and privacyPolicyUrl accept either url string or a callback
       // function.
-      signInSuccessUrl: "/",
+      signInSuccessUrl: "/home",
       // Terms of service url/callback.
       tosUrl: "/tos/",
       // Privacy policy url/callback.
@@ -36,43 +32,4 @@ export async function startUiAuth() {
     // The start method will wait until the DOM is loaded.
     ui.start("#firebaseui-auth-container", uiConfig);
   }
-}
-
-export function checkUserState(setUid: SetterOrUpdater<string>) {
-  firebase.auth().onAuthStateChanged((user) => {
-    if (user) {
-      setUid(user.uid);
-
-      if (localStorage.getItem("database") === "undefined") {
-        console.log("作ったで");
-        createNewLocalDB();
-      }
-
-      const userDB = new FastNoteDatabase(user.uid);
-      userDB
-        .syncDB()
-        .then(() => {
-          router.push("/home");
-        })
-        .catch((e) => {
-          if (e.message === "NotFoundRemoteDB") {
-            userDB
-              .createNewDB()
-              .then(() => {
-                router.push("/home");
-              })
-              .catch((e) => {
-                alert(
-                  `データベース作成中にエラーが発生しました。 \n${e.message}`
-                );
-              });
-          } else {
-            alert(`データ取得中に下記のエラーが発生しました。 \n${e.message}`);
-            router.reload();
-          }
-        });
-    } else {
-      router.push("/");
-    }
-  });
 }
