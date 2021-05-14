38a6b689cfedef7f8e44db65103b0c6d39d8bb75
ippee <ippee.music@gmail.com>
2021-05-14 11:27:25 +0900

    selectorの削除

--- src/lib/atoms/UserIdAtoms.ts
+++ src/lib/atoms/UserIdAtoms.ts
@@ -1,26 +1,6 @@
 import { atom, selector, useSetRecoilState } from "recoil";
-import firebase from "firebase/app";
-import "firebase/auth";
 
-const uidOriginState = atom({
-  key: "uidOriginState",
-  default: "",
-});
-
-export const uidState = selector({
+export const uidState = atom({
   key: "uidState",
-  get: ({ get }) => {
-    const uid = get(uidOriginState);
-
-    if (uid === "") {
-      const user = firebase.auth().currentUser;
-      const setUid = useSetRecoilState(uidOriginState);
-      setUid(user.uid);
-
-      return user.uid;
-    } else {
-      return uid;
-    }
-  },
-  set: ({ set }, uid: string) => set(uidOriginState, uid),
+  default: "",
 });
