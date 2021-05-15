547002f3751ca2c12b040f342571b330fd5d8596
ippee <ippee.music@gmail.com>
2021-05-15 12:54:59 +0900

    使用しないatomの削除

--- src/lib/atoms/editorAtoms.ts
+++ src/lib/atoms/editorAtoms.ts
@@ -1,21 +1,5 @@
 import { atom } from "recoil";
 
-import { Memo } from "../databaseInfo";
-
-// 選択中のメモの情報
-export const currentMemoState = atom({
-  key: "currentMemoState",
-  default: {
-    title: "",
-    category: "",
-    tags: [""],
-    star: false,
-    created: "",
-    updated: "",
-    content: "",
-  } as Memo,
-});
-
 // 現在選択中のメモのインデックス
 export const memoIndexState = atom({
   key: "memoIndexState",
