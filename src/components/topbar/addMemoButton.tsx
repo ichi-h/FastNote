f2549e67f6a7db70166c43119aecf29ebea5222a
ippee <ippee.music@gmail.com>
2021-05-15 12:47:35 +0900

    メモ追加のメソッドを仮組み, #26

--- src/components/topbar/addMemoButton.tsx
+++ src/components/topbar/addMemoButton.tsx
@@ -1,8 +1,64 @@
+import { useSetRecoilState, useRecoilValue } from "recoil";
 import { css } from "styled-jsx/css";
+import firebase from "firebase/app";
+
+import "firebase/database";
+
+import { FastNoteDate } from "../../lib/fastNoteDate";
+import { memoIndexState } from "../../lib/atoms/editorAtoms";
+import { uidState } from "../../lib/atoms/userIdAtoms";
 
 export default function AddMemoButton() {
-  const handleClick = () => {
+  const setIndex = useSetRecoilState(memoIndexState);
+  const userUid = useRecoilValue(uidState);
 
+  const handleClick = () => {
+    const addNewMemo = () => {
+      return new Promise<object>((resolve) => {
+        const fnd = new FastNoteDate();
+
+        const newMemo = {
+          title: "新しいメモ",
+          category: "None",
+          tags: [""],
+          star: false,
+          created: fnd.getCurrentDate(),
+          updated: fnd.getCurrentDate(),
+          content: "",
+        };
+        let localDB = JSON.parse(localStorage.getItem("database"));
+        const nextIndex: string = String(Object.keys(localDB.memos).length);
+
+        localDB.memos[nextIndex] = newMemo;
+
+        console.log({
+          nextIndex: nextIndex,
+          localDB: localDB,
+        });
+
+        resolve({
+          nextIndex: nextIndex,
+          localDB: localDB,
+        });
+      });
+    };
+
+    const changeTarget = async (nextIndex: string) => {
+      setIndex(nextIndex);
+    };
+
+    const pushToRemoteDB = (localDB: object) => {
+      return new Promise((_, reject) => {
+        firebase.database().ref(`users/${userUid}`).set(localDB).catch((e) => reject(e));
+      });
+    };
+
+    addNewMemo().then(async (res) => {
+      await changeTarget(res["nextIndex"]);
+      await pushToRemoteDB(res["localDB"]).catch((e) => {
+        alert(`下記の理由によりクラウド上にメモをアップロードできませんでした。 \n${e}`);
+      });
+    });
   };
 
   return (
