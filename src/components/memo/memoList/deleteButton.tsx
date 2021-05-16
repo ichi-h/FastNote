083b7153fad6e38c82c8916c790ade807bf0955f
ippee <ippee.music@gmail.com>
2021-05-16 21:40:52 +0900

    削除したメモのインデックスが割り振られないよう修正, #55

--- src/components/memo/memoList/deleteButton.tsx
+++ src/components/memo/memoList/deleteButton.tsx
@@ -1,12 +1,22 @@
 import { useRecoilState } from "recoil";
 
 import { localDBState } from "../../../lib/atoms/localDBAtom";
+import { memoIndexState } from "../../../lib/atoms/editorAtoms";
 
 export default function DeleteButton(props: { index: number }) {
+  const [memoIndex, setIndex] = useRecoilState(memoIndexState);
   const [localDBStr, setLocalDB] = useRecoilState(localDBState);
   let localDB = JSON.parse(localDBStr);
 
   const handleClick = () => {
+    if (Number(memoIndex) === props.index) {
+      const keys = Object.keys(localDB.memos).map((value) => Number(value));
+      const maxValue = keys.reduce((pre, cur) => {
+        return Math.max(pre, cur);
+      });
+      setIndex(String(maxValue));
+    }
+
     delete localDB.memos[props.index];
     setLocalDB(JSON.stringify(localDB));
   };
