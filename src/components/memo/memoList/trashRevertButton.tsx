6aaf2b94ea21348b637a8c4be212b020a9a24dd0
ippee <ippee.music@gmail.com>
2021-05-16 16:45:11 +0900

    TrashButtonとRevertButtonを共通化, #48

---
+++ src/components/memo/memoList/trashRevertButton.tsx
@@ -0,0 +1,32 @@
+import { useRecoilState } from "recoil";
+
+import { localDBState } from "../../../lib/atoms/localDBAtom";
+
+type FuncType = "trash" | "revert";
+
+export default function TrashRevertButton(props: { func: FuncType, index: number }) {
+  const trashAtr = (func: FuncType) => {
+    switch (func) {
+      case "trash":
+        return true;
+      case "revert":
+        return false;
+    }
+  };
+
+  const [localDBStr, setLocalDB] = useRecoilState(localDBState);
+  let localDB = JSON.parse(localDBStr);
+
+  const handleClick = () => {
+    localDB.memos[props.index].trash = trashAtr(props.func);
+    setLocalDB(JSON.stringify(localDB));
+  };
+
+  return (
+    <>
+      <div className="trash-revert-button">
+        <button onClick={handleClick}>箱</button>
+      </div>
+    </>
+  );
+}
