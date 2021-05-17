80dccdfeeed1ab6d63fccded350bdcd98df77375
ippee <ippee.music@gmail.com>
2021-05-17 17:02:31 +0900

    addCategoryButtonコンポーネントの追加, #25

---
+++ src/components/navbar/addCategoryButton.tsx
@@ -0,0 +1,17 @@
+import { css } from "styled-jsx/css";
+
+export default function AddCategoryButton() {
+  return (
+    <>
+      <div className="add-category-button"></div>
+
+      <style jsx>{addCategoryButtonStyle}</style>
+    </>
+  );
+}
+
+const addCategoryButtonStyle = css`
+  .add-category-button {
+
+  }
+`;
