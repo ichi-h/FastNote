408ad39cded211acbf7b6f003c9f91c29c9f4b6e
ippee <ippee.music@gmail.com>
2021-05-17 19:07:12 +0900

    キー入力のイベントハンドラを追加, #25

--- src/components/navbar/categoryInput.tsx
+++ src/components/navbar/categoryInput.tsx
@@ -14,11 +14,24 @@ export default function CategoryInput() {
 
   const handleClick = () => toggleIsShow(false);
 
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+
+      const categoryName = e.currentTarget.value;
+
+      if (categoryName !== "") {
+
+      }
+
+      toggleIsShow(false);
+    }
+  };
+
   if (inputIsShow) {
     return (
       <>
         <div className="category-input">
-          <input type="text" name="" id="" ref={inputRef} />
+          <input type="text" name="input" onKeyDown={handleKeyDown} ref={inputRef} />
           <button onClick={handleClick}>×</button>
         </div>
         <style jsx>{categoryInputStyle}</style>
