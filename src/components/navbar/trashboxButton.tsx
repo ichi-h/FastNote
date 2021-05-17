2bb51ff166d1cfcf2ec0310ef9bcd744ea241e50
ippee <ippee.music@gmail.com>
2021-05-17 21:57:11 +0900

    ごみ箱内のメモの数をカウントする機能を追加, close #61

--- src/components/navbar/trashboxButton.tsx
+++ src/components/navbar/trashboxButton.tsx
@@ -1,17 +1,39 @@
 import { Link } from "react-router-dom";
 import { css } from "styled-jsx/css";
-import { useSetRecoilState } from "recoil";
+import { useSetRecoilState, useRecoilValue } from "recoil";
 
 import {
   currentCategoryState,
   openNavbarState,
   trashboxState,
 } from "../../lib/atoms/uiAtoms";
+import { localDBState } from "../../lib/atoms/localDBAtom";
+
+function countTrashedMemos(memos: any) {
+  let count = 0;
+
+  if (memos) {
+    count = Object.entries(memos)
+      .map(([_, memo]: [string, any]) => memo)
+      .reduce((pre: number, memo) => {
+        if (memo.trash) {
+          pre += 1;
+        }
+        return pre;
+      }, 0);
+  }
+
+  return count;
+}
 
 export default function TrashboxButton() {
   const toggleNav = useSetRecoilState(openNavbarState);
   const toggleTrash = useSetRecoilState(trashboxState);
   const setCategory = useSetRecoilState(currentCategoryState);
+
+  const localDB = JSON.parse(useRecoilValue(localDBState));
+
+  const count = countTrashedMemos(localDB.memos);
 
   const handleClick = () => {
     toggleNav(false);
@@ -23,7 +45,7 @@ export default function TrashboxButton() {
     <>
       <Link to="/home">
         <div className="trashbox-button" onClick={handleClick}>
-          ごみ箱
+          ごみ箱 ({count})
         </div>
       </Link>
 
