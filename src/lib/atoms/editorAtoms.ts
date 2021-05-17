7ea097bf602fddac4d704e65d87f5bc867af15bf
ippee <ippee.music@gmail.com>
2021-05-17 16:09:17 +0900

    setIndex時の探索を削除 => atomによる管理に戻す, #58

--- src/lib/atoms/editorAtoms.ts
+++ src/lib/atoms/editorAtoms.ts
@@ -1,71 +1,71 @@
-import { atom, selector } from "recoil";
+import { atom } from "recoil";
 
 // 現在選択中のメモのインデックス
-const memoIndexOriginState = atom({
-  key: "memoIndexOriginState",
+export const memoIndexState = atom({
+  key: "memoIndexState",
   default: "-1",
 });
 
-export const memoIndexState = selector<string>({
-  key: "memoIndexState",
-  get: ({ get }) => {
-    return get(memoIndexOriginState);
-  },
-  set: ({ set }, inputValue) => {
-    console.log("set");
+// export const memoIndexState = selector<string>({
+//   key: "memoIndexState",
+//   get: ({ get }) => {
+//     return get(memoIndexOriginState);
+//   },
+//   set: ({ set }, inputValue) => {
+//     console.log("set");
 
-    const localDB = JSON.parse(localStorage.getItem("database"));
-    const keys = Object.keys(localDB.memos).map((value) => Number(value));
+//     const localDB = JSON.parse(localStorage.getItem("database"));
+//     const keys = Object.keys(localDB.memos).map((value) => Number(value));
 
-    const res = binSearch(Number(inputValue), keys);
-    console.log(res);
+//     const res = binSearch(Number(inputValue), keys);
+//     console.log(res);
 
-    if (res === "NotFound") {
-      set(memoIndexOriginState, getlatestMemoIndex(keys, localDB));
-    } else {
-      set(memoIndexOriginState, inputValue);
-    }
-  },
-});
+//     if (res === "NotFound") {
+//       set(memoIndexOriginState, getlatestMemoIndex(keys, localDB));
+//     } else {
+//       set(memoIndexOriginState, inputValue);
+//     }
+//   },
+// });
 
-const binSearch = (target: number, array: number[]) => {
-  let res = "NotFound";
-  let head = 0;
-  let tail = array.length;
+// const binSearch = (target: number, array: number[]) => {
+//   let res = "NotFound";
+//   let head = 0;
+//   let tail = array.length;
 
-  console.log(target);
-  console.log(array);
+//   console.log(target);
+//   console.log(array);
 
-  while (head <= tail) {
-    let center = Math.floor((head + tail) / 2);
+//   while (head <= tail) {
+//     let center = Math.floor((head + tail) / 2);
 
-    console.log(
-      `head: ${head}, tail: ${tail}, center: ${center}, array[center]: ${array[center]}`
-    );
+//     console.log(
+//       `head: ${head}, tail: ${tail}, center: ${center}, array[center]: ${array[center]}`
+//     );
 
-    if (target === array[center]) {
-      res = String(array[center]);
-      break;
-    }
+//     if (target === array[center]) {
+//       res = String(array[center]);
+//       break;
+//     }
 
-    if (array[center] < target) {
-      head = center + 1;
-    } else if (target < array[center]) {
-      tail = center - 1;
-    }
-  }
+//     if (array[center] < target) {
+//       head = center + 1;
+//     } else if (target < array[center]) {
+//       tail = center - 1;
+//     }
+//   }
 
-  return res;
-};
+//   return res;
+// };
 
-const getlatestMemoIndex = (keys: number[], localDB: any) => {
-  const latestMemoIndex = keys.reduce((pre, cur) => {
-    const preUpdated = Number(localDB.memos[pre].updated);
-    const curUpdated = Number(localDB.memos[cur].updated);
+// const getlatestMemoIndex = (keys: number[], localDB: any) => {
+//   const latestMemoIndex = keys.reduce((pre, cur) => {
+//     const preUpdated = Number(localDB.memos[pre].updated);
+//     const curUpdated = Number(localDB.memos[cur].updated);
 
-    if (preUpdated < curUpdated) return cur;
-    else return pre;
-  }, Math.min(...keys));
+//     if (preUpdated < curUpdated) return cur;
+//     else return pre;
+//   }, Math.min(...keys));
 
-  return String(latestMemoIndex);
-};
+//   return String(latestMemoIndex);
+// };
