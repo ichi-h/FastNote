feb22cb3fb1a655b01bc164eabee8b7b8da53edb
ippee <ippee.music@gmail.com>
2021-05-18 18:25:02 +0900

    ハンドラをPromise化, #49

--- src/components/navbar/ellipse/handler.ts
+++ src/components/navbar/ellipse/handler.ts
@@ -3,40 +3,46 @@ interface HandlerProps {
   e: React.MouseEvent<HTMLButtonElement, MouseEvent>
 }
 
-export type Handler = (props: HandlerProps) => void;
+export type Handler = (props: HandlerProps) => Promise<unknown>;
 
 export const deleteCategory: Handler = (props: HandlerProps) => {
-  const target = props.e.currentTarget.value;
+  return new Promise((resolve) => {
+    const target = props.e.currentTarget.value;
 
-  const trashMemos = () => {
-    if (props.localDB.memos) {
-      const keys = Object.keys(props.localDB.memos);
-      const selectedKeys = keys.filter(
-        (key) => props.localDB.memos[key].category === target
-      );
+    const trashMemos = async () => {
+      if (props.localDB.memos) {
+        const keys = Object.keys(props.localDB.memos);
+        const selectedKeys = keys.filter(
+          (key) => props.localDB.memos[key].category === target
+        );
 
-      for (let key of selectedKeys) {
-        props.localDB.memos[key].category = "None";
-        props.localDB.memos[key].trash = true;
+        for (let key of selectedKeys) {
+          props.localDB.memos[key].category = "None";
+          props.localDB.memos[key].trash = true;
+        }
       }
-    }
-  };
+    };
 
-  const delCat = () => {
-    const categoryList = Object.entries(props.localDB.categories)
-      .map(([_, category]: [string, string]) => category)
-      .filter((category) => category !== target);
+    const delCat = async () => {
+      const categoryList = Object.entries(props.localDB.categories)
+        .map(([_, category]: [string, string]) => category)
+        .filter((category) => category !== target);
 
-    const categoryObj = categoryList.reduce((pre, cur, i) => {
-      pre[i] = cur;
-      return pre;
-    }, {});
+      const categoryObj = categoryList.reduce((pre, cur, i) => {
+        pre[i] = cur;
+        return pre;
+      }, {});
 
-    props.localDB.categories = categoryObj;
-  };
+      props.localDB.categories = categoryObj;
+    };
 
-  trashMemos();
-  delCat();
+    const process = async () => {
+      await trashMemos();
+      await delCat();
+    };
+
+    process().then(resolve);
+  });
 };
 
 export const renameCategory = (
