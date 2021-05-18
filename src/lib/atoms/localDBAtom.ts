37d18b552e7417e689ed15ed7f24937df631829c
ippee <ippee.music@gmail.com>
2021-05-18 21:57:56 +0900

    localDBの更新にメモのソートを追加, close #38

--- src/lib/atoms/localDBAtom.ts
+++ src/lib/atoms/localDBAtom.ts
@@ -21,10 +21,11 @@ export const localDBState = selector({
     return get(localDBOriginState);
   },
   set: ({ set }, inputValue: string) => {
+    let localDB = JSON.parse(inputValue);
+
     const updateLocalDB = async (currentDate: number) => {
       localStorage.setItem("database", inputValue);
 
-      let localDB = JSON.parse(inputValue);
       localDB.lastUpdated = currentDate;
 
       set(localDBOriginState, JSON.stringify(localDB));
@@ -47,9 +48,39 @@ export const localDBState = selector({
       });
     };
 
+    const sort = async () => {
+      if (localDB.memos) {
+        const keys = Object.keys(localDB.memos);
+
+        let tmp = Number.MAX_SAFE_INTEGER;
+        const sortedKeyList = keys.reduce((sorted, key) => {
+          const updated = Number(localDB.memos[key].updated);
+          if (updated <= tmp) {
+            tmp = updated;
+            sorted = sorted.concat(key);
+          } else {
+            const index = () => {
+              for (let i = 0; i < sorted.length; i++) {
+                if (localDB.memos[sorted[i]].updated <= updated) {
+                  if (i === 0) return 0;
+                  else return i - 1;
+                }
+              }
+            };
+            sorted.splice(index(), 0, key);
+          }
+          return sorted;
+        }, []);
+
+        localDB.memos = sortedKeyList.reduce((pre, key, i) => {
+          pre[i] = localDB.memos[key];
+          return pre;
+        }, {});
+      }
+    };
+
     const update = (currentDate: number) => {
       return new Promise((resolve, reject) => {
-        let localDB = JSON.parse(inputValue);
         localDB.lastUpdated = currentDate;
 
         localStorage.setItem("database", JSON.stringify(localDB));
@@ -72,6 +103,7 @@ export const localDBState = selector({
       await updateLocalDB(currentDate);
       await sleep(2000);
       await checkDifference();
+      await sort();
       await update(currentDate);
     };
 
