bde23c2a30e671b2503395d84d777661f822a3ae
ippee <ippee.music@gmail.com>
2021-05-18 17:33:17 +0900

    カテゴリー削除時の機能を実装, #49

--- src/components/navbar/ellipse/handler.ts
+++ src/components/navbar/ellipse/handler.ts
@@ -1,6 +1,34 @@
-export const deleteCategory = (
-  e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-) => {};
+export const deleteCategory = (localDB: any, target: string) => {
+    const trashMemos = () => {
+      if (localDB.memos) {
+        const keys = Object.keys(localDB.memos);
+        const selectedKeys = keys.filter(
+          (key) => localDB.memos[key].category === target
+        );
+
+        for (let key of selectedKeys) {
+          localDB.memos[key].category = "None";
+          localDB.memos[key].trash = true;
+        }
+      }
+    };
+
+    const delCat = () => {
+      const categoryList = Object.entries(localDB.categories)
+        .map(([_, category]: [string, string]) => category)
+        .filter((category) => category !== target);
+
+      const categoryObj = categoryList.reduce((pre, cur, i) => {
+        pre[i] = cur;
+        return pre;
+      }, {});
+
+      localDB.categories = categoryObj;
+    };
+
+    trashMemos();
+    delCat();
+};
 
 export const renameCategory = (
   e: React.MouseEvent<HTMLButtonElement, MouseEvent>
