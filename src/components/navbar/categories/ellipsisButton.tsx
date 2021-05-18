f38c935e5f5e9bd3a401831a452538df14420194
ippee <ippee.music@gmail.com>
2021-05-18 15:23:53 +0900

    メニュー外クリック時にメニューが閉じるよう修正, #62

--- src/components/navbar/categories/ellipsisButton.tsx
+++ src/components/navbar/categories/ellipsisButton.tsx
@@ -1,14 +1,38 @@
 import { useState } from "react";
 import { css } from "styled-jsx/css";
 
+import PulldownMenu from "../pulldownMenu";
+
 export default function EllipsisButton() {
   const [menuIsShow, toggleIsShow] = useState(false);
 
-  const handleClick = () => toggleIsShow(!menuIsShow);
+  const showMenu = () => {
+    if (menuIsShow) {
+      return (
+        <PulldownMenu
+          item={[{ name: "削除", handler: removeCategory, index: 0 }]}
+          isShow={menuIsShow}
+          dispatch={toggleIsShow}
+        />
+      );
+    }
+  };
+
+  const handleClick = () => {
+    toggleIsShow(!menuIsShow);
+  };
+
+  const removeCategory = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    console.log("削除するぞい" + e.currentTarget.value);
+    toggleIsShow(false);
+  }
 
   return (
     <>
-      <button className="ellipsis" onClick={handleClick}>3</button>
+      <div className="ellipsis">
+        <button className="ellipsis-button" onClick={handleClick}>3</button>
+        {showMenu()}
+      </div>
 
       <style jsx>{ellipsisButtonStyle}</style>
     </>
@@ -16,7 +40,7 @@ export default function EllipsisButton() {
 }
 
 const ellipsisButtonStyle = css`
-  .ellipsis {
+  .ellipsis-button {
     position: absolute;
     top: 50%;
     right: 1rem;
--- src/components/navbar/pulldownMenu.tsx
+++ src/components/navbar/pulldownMenu.tsx
@@ -1,20 +1,47 @@
+import { useEffect, useRef } from "react";
 import { css } from "styled-jsx/css";
 
 interface MenuItem {
   name: string;
-  handler: () => void;
-  index?: number;
+  handler: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  index: number;
 }
 
 interface PulldownMenuProps {
   item: MenuItem[];
   isShow: boolean;
+  dispatch: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
 export default function PulldownMenu(props: PulldownMenuProps) {
+  const menuRef: React.RefObject<HTMLDivElement> = useRef();
+
+  const handleBlur = () => {
+    props.dispatch(false);
+  };
+
+  useEffect(() => {
+    menuRef.current.focus();
+  });
+
   return (
     <>
-      <div className="pulldown-menu"></div>
+      <div className="pulldown-menu" ref={menuRef} onBlur={handleBlur} tabIndex={0}>
+        {
+          props.item.map((menuIteml) => {
+            return (
+              <button
+                className={`pulldown-item-${menuIteml.index}`}
+                key={`pulldown-item-${menuIteml.index}`}
+                value={menuIteml.index}
+                onClick={menuIteml.handler}
+              >
+                {menuIteml.name}
+              </button>
+            );
+          })
+        }
+      </div>
       <style jsx>{pulldownMenuStyle}</style>
     </>
   );
@@ -22,6 +49,14 @@ export default function PulldownMenu(props: PulldownMenuProps) {
 
 const pulldownMenuStyle = css`
   .pulldown-menu {
-
+    position: absolute;
+    bottom: -2rem;
+    right: -8rem;
+    background-color: white;
+    border: 1px solid #000;
+    font-size: 2rem;
+    width: 10rem;
+    z-index: 10000;
+    text-align: center;
   }
 `;
