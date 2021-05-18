56a6ff9746931d238fe93e2dd8e5264e662e2ae9
ippee <ippee.music@gmail.com>
2021-05-18 17:13:01 +0900

    プルダウンメニューの項目選択時に発火するイベントを定義, #48

---
+++ src/components/navbar/ellipse/handler.ts
@@ -0,0 +1,11 @@
+export const deleteCategory = (
+  e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+) => {};
+
+export const renameCategory = (
+  e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+) => {};
+
+export const deleteTrashedMemos = (
+  e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+) => {};
