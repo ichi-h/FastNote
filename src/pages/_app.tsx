d18f168ae456d6357c4a4c97ebae3a26441106cd
ippee <ippee.music@gmail.com>
2021-05-22 14:45:31 +0900

    manifestの追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,6 +12,7 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
       <Head>
         <title>Fast Note</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@300&display=swap"
