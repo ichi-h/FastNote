754eb67715fcafdc3197ac9c269fe371dfec6a80
ippee <ippee.music@gmail.com>
2021-05-30 20:43:19 +0900

    localDBStateの副作用をatomに移動, #63

--- src/lib/atoms/localDBAtom.ts
+++ src/lib/atoms/localDBAtom.ts
@@ -9,6 +9,65 @@ import { FastNoteDate } from "../fastNoteDate";
 const localDBOriginState = atom({
   key: "localDBOriginState",
   default: "",
+  effects_UNSTABLE: [
+    ({ onSet }) => {
+      onSet((inputValue: string) => {
+        let localDB = JSON.parse(inputValue);
+
+        const cacheLocalDB = async () => {
+          sessionStorage.setItem("dbCache", inputValue);
+        };
+
+        const sleep = (ms: number) => {
+          return new Promise((resolve) => {
+            setTimeout(resolve, ms);
+          });
+        };
+
+        const checkDifference = () => {
+          return new Promise((resolve, reject) => {
+            const cachedLocalDB = JSON.parse(sessionStorage.getItem("dbCache"));
+
+            const before = localDB.lastUpdated;
+            const after = cachedLocalDB.lastUpdated;
+
+            if (before === after) {
+              resolve("データベースの更新が停止");
+            } else {
+              reject("データベースの更新は続行");
+            }
+          });
+        };
+
+        const update = () => {
+          return new Promise((resolve, reject) => {
+            sessionStorage.setItem("dbCache", undefined);
+            const uid = firebase.auth().currentUser.uid;
+
+            firebase
+              .database()
+              .ref(`users/${uid}`)
+              .set(localDB)
+              .then(() => resolve("データベースを更新"))
+              .catch((e) => reject(e));
+          });
+        };
+
+        const process = async () => {
+          await cacheLocalDB();
+          await sleep(2000);
+          await checkDifference();
+          await update();
+        };
+
+        process().catch((e) => {
+          if (e !== "データベースの更新は続行") {
+            alert(`データベース更新中にエラーが発生しました。 \n${e}`);
+          }
+        });
+      });
+    },
+  ],
 });
 
 export const localDBState = selector({
@@ -19,62 +78,9 @@ export const localDBState = selector({
   set: ({ set }, inputValue: string) => {
     let localDB = JSON.parse(inputValue);
 
-    const updateLocalDB = async (currentDate: number) => {
-      localDB.lastUpdated = currentDate;
+    const fnd = new FastNoteDate();
+    localDB.lastUpdated = fnd.getCurrentDate();
 
-      set(localDBOriginState, JSON.stringify(localDB));
-      sessionStorage.setItem("dbCache", JSON.stringify(localDB));
-    };
-
-    const sleep = (ms: number) => {
-      return new Promise((resolve) => {
-        setTimeout(resolve, ms);
-      });
-    };
-
-    const checkDifference = () => {
-      return new Promise((resolve, reject) => {
-        const newLocalDB = JSON.parse(sessionStorage.getItem("dbCache"));
-
-        const before = localDB.lastUpdated;
-        const after = newLocalDB.lastUpdated;
-
-        if (before === after) {
-          resolve("データベースの更新が停止");
-        } else {
-          reject("データベースの更新は続行");
-        }
-      });
-    };
-
-    const update = () => {
-      return new Promise((resolve, reject) => {
-        sessionStorage.setItem("dbCache", undefined);
-        const uid = firebase.auth().currentUser.uid;
-
-        firebase
-          .database()
-          .ref(`users/${uid}`)
-          .set(localDB)
-          .then(() => resolve("データベースを更新"))
-          .catch((e) => reject(e));
-      });
-    };
-
-    const process = async () => {
-      const fnd = new FastNoteDate();
-      const currentDate = fnd.getCurrentDate();
-
-      await updateLocalDB(currentDate);
-      await sleep(2000);
-      await checkDifference();
-      await update();
-    };
-
-    process().catch((e) => {
-      if (e !== "データベースの更新は続行") {
-        alert(`データベース更新中にエラーが発生しました。 \n${e}`);
-      }
-    });
+    set(localDBOriginState, JSON.stringify(localDB));
   },
 });
