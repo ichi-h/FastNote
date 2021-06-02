1eee7677eaec7e4711afe40db9b7cc2962df8de8
ippee <ippee.music@gmail.com>
2021-06-02 17:09:32 +0900

    ソートされたキーを返すように修正, #98

--- src/lib/sort.ts
+++ src/lib/sort.ts
@@ -1,42 +1,34 @@
 import { dateInfoToNum } from "./fastNoteDate";
 
-export async function insertionSort(
-  localDB: any,
-  callback: (localDBStr: string) => void
-) {
-  const sort = async () => {
-    if (localDB.memos) {
-      const keys = Object.keys(localDB.memos);
+export function insertionSort(localDB: any): string[] {
+  if (localDB.memos) {
+    const keys = Object.keys(localDB.memos);
 
-      let tmp = Number.MAX_SAFE_INTEGER;
-      const sortedKeyList = keys.reduce((sorted, key) => {
-        const updated = dateInfoToNum(localDB.memos[key].updated);
+    let tmp = Number.MAX_SAFE_INTEGER;
+    const sortedKeyList = keys.reduce((sorted, key) => {
+      const updated = dateInfoToNum(localDB.memos[key].updated);
 
-        if (updated <= tmp) {
-          tmp = updated;
-          sorted = sorted.concat(key);
-        } else {
-          const index = () => {
-            for (let i = 0; i < sorted.length; i++) {
-              if (localDB.memos[sorted[i]].updated <= updated) {
-                if (i === 0) return 0;
-                else return i - 1;
-              }
+      if (updated <= tmp) {
+        tmp = updated;
+        sorted = sorted.concat(key);
+      } else {
+        const index = () => {
+          for (let i = 0; i < sorted.length; i++) {
+            const updated2 = dateInfoToNum(localDB.memos[sorted[i]].updated);
+            if (updated2 <= updated) {
+              if (i === 0) return 0;
+              else return i - 1;
             }
-          };
-          sorted.splice(index(), 0, key);
-        }
-        return sorted;
-      }, []);
+          }
+        };
+        sorted.splice(index(), 0, key);
+      }
 
-      localDB.memos = sortedKeyList.reduce((pre, key, i) => {
-        pre[i] = localDB.memos[key];
-        return pre;
-      }, {});
-    }
-  };
+      return sorted;
+    }, []);
 
-  await sort().then(() => {
-    callback(JSON.stringify(localDB));
-  });
+    return sortedKeyList;
+  }
+
+  return [];
 }
