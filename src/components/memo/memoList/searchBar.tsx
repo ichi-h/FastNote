8fc40a99e91dfa849ca7147a7ce8e3bad44ca059
ippee <ippee.music@gmail.com>
2021-06-03 15:11:38 +0900

    SearchBarコンポーネントの追加, #52

---
+++ src/components/memo/memoList/searchBar.tsx
@@ -0,0 +1,17 @@
+import { css } from 'styled-jsx/css';
+
+export default function SearchBar() {
+  return (
+    <>
+      <div className='searchbar'>
+
+      </div>
+
+      <style jsx>{SearchBarStyle}</style>
+    </>
+  );
+}
+
+const SearchBarStyle = css`
+  .searchbar {}
+`;
