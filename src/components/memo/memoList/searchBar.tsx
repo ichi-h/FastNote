6926182c82c004df21245b083546f28702e9fdc5
ippee <ippee.music@gmail.com>
2021-06-03 16:04:30 +0900

    SearchBarの機能を実装, #52

--- src/components/memo/memoList/searchBar.tsx
+++ src/components/memo/memoList/searchBar.tsx
@@ -1,10 +1,25 @@
+import { useRecoilState } from 'recoil';
 import { css } from 'styled-jsx/css';
 
+import { searchKeywordState } from '../../../lib/atoms/uiAtoms';
+
 export default function SearchBar() {
+  const [keyword, setKeyword] = useRecoilState(searchKeywordState);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(e.currentTarget.value);
+  };
+
   return (
     <>
       <div className='searchbar'>
-
+        <input
+          type="text"
+          name="search"
+          placeholder="メモを検索"
+          defaultValue={keyword}
+          onChange={handleChange}
+        />
       </div>
 
       <style jsx>{SearchBarStyle}</style>
