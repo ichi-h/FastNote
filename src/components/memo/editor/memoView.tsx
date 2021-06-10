afbb71a73f52ddeed1c5855f7fe5d6ce4dace13e
ippee <ippee.music@gmail.com>
2021-06-10 18:49:53 +0900

    MemoViewコンポーネントの追加, #21

---
+++ src/components/memo/editor/memoView.tsx
@@ -0,0 +1,17 @@
+import { css } from 'styled-jsx/css';
+
+export default function MemoView() {
+  return (
+    <>
+      <div className='memo-view'>
+
+      </div>
+
+      <style jsx>{memoViewStyle}</style>
+    </>
+  );
+}
+
+const memoViewStyle = css`
+  .memo-view {}
+`;
